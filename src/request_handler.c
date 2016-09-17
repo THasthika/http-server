48782e8316119f86533f6b06a3301154d6c42bdd
Tharindu Hasthika <tharindubathigama@gmail.com>
2016-09-17 18:57:38 +0530

    update #1

--- src/request_handler.c
+++ src/request_handler.c
@@ -2,36 +2,43 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "request_handler.h"
+
+#include "dstring.h"
+
+#define BUFFER_SIZE 512
 
 void handle_request(int fd)
 {
 	ssize_t count;
-	char buffer[512];
-	int done = 0;
+	char buffer[BUFFER_SIZE];
+	DString *str = dstring_new();
+
 	while(1) {
-		if((count = read(fd, buffer, sizeof buffer)) == -1) {
+		if((count = read(fd, buffer, sizeof(buffer))) == -1) {
 			if(errno != EAGAIN) {
 				perror("read");
-				done = 1;
+				goto cleanup;
 			}
 			break;
 		}
 		if(count == 0) {
-			done = 1;
 			break;
 		}
-		printf("%s\n", buffer);
-		if(write(fd, buffer, count) == -1) {
-			perror("write");
-			done = 1;
-			break;
-		}
+		dstring_nappend(str, buffer, count);
 	}
 
-	if(done) {
-		printf("closing connection %d\n", fd);
-		close(fd);
-	}
+	char *request_raw = dstring_to_string(str);
+
+	printf("%s\n", request_raw);
+
+	send(fd, request_raw, strlen(request_raw), 0);
+
+	printf("closing connection %d\n", fd);
+	close(fd);
+
+cleanup:
+	dstring_free(str);
 }
--- test/http_request_parser.c
+++ test/http_request_parser.c
@@ -1,13 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <dstring.h>
 
 #define REQUEST_FILE "http_request.raw"
+#define BUFFER_SIZE 512
 
 int main(int argc, char **argv)
 {
-	FILE *fp = fopen(REQUEST_FILE, "r");
+	char buffer[BUFFER_SIZE];
+
+	DString *string = dstring_new();
+
+	int fd = open(REQUEST_FILE, 0, O_RDONLY);
+
+	int rc = 0;
+
+	while((rc = read(fd, buffer, BUFFER_SIZE)) > 0) {
+		dstring_nappend(string, buffer, rc);
+	}
+
+	char *x = dstring_to_string(string);
+
+	printf("%s\n", x);
+
+	free(x);
 
 
-
-	fclose(fp);
+	dstring_free(string);
+	close(fd);
 }
